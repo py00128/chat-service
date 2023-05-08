be8c1588bef84f9a29391fed92b6200ec0d372ad
py00128 <py00128@surrey.ac.uk>
2023-05-08 15:37:01 +0100

    added the changes needed to adapt with main app

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,28 @@ const cors = require('cors')
 const app = express();
 
 //connect to our database
-mongoose.connect(process.env.DATABASE_URL);
+const uri = "mongodb+srv://dbChat:group12@cluster0.gochocx.mongodb.net/Chat?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.error("Connected to MongoDB"));
 
 
 //adding middleware cors
+
+const userConversationRouter = require('./routes/userConversations')
+//const authMiddleware = require("./middleware/authMiddleware");
+
 app.use(cors({
     origin: 'http://localhost:8080'
   }));
-  
+
 // makes our app accept json
 app.use(express.json())
-
+//app.use(authMiddleware)
 // importing route so we can redirect api calls
 const inboxRouter = require('./routes/inbox')
 app.use('/inbox', inboxRouter)
-
-const userConversationRouter = require('./routes/userConversations')
 app.use('/userConversations',userConversationRouter)
 
 // this port is listening for any request from the front-end
